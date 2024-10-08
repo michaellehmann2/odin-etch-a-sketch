c37c7d8463bcbdba6d97eae5f0bcbf141707c592
Michael Lehmann <mikelehmann00@gmail.com>
2024-10-08 14:46:42 -0500

    Add reset/change square count functionality
    Added a button which clears the grid and changes the number of squares-
    per-side of the grid.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,4 @@
-let squareDimension = 16;
+const INIT_SIDE_LENGTH = 16;
 let currentCursorColor = "grey";
 
 function drawSquareGrid(sideLength) {
@@ -18,4 +18,20 @@ function drawSquareGrid(sideLength) {
     }
 }
 
-drawSquareGrid(squareDimension);
+function changeNumSquares() {
+    const container = document.querySelector(".container");
+    //This only works for more modern browsers. It might not work if you're using an older version. Maybe I should address this later?
+    container.replaceChildren();
+
+    let newSquareCount;
+    do {
+        newSquareCount = prompt("Please enter a new number of squares-per-side of the grid. Make sure to enter a number between 1 and 100.", 16);
+    }  while (newSquareCount == null || (newSquareCount > 100 || newSquareCount < 1));
+
+    drawSquareGrid(newSquareCount);
+}
+
+drawSquareGrid(INIT_SIDE_LENGTH);
+
+const resetChangeNumButton = document.querySelector(".reset-change-num-button");
+resetChangeNumButton.addEventListener("click", changeNumSquares);
