9a417f3f91055b06d0244b0abc8debba8dd6dec5
Michael Lehmann <mikelehmann00@gmail.com>
2024-10-08 14:18:16 -0500

    Add etch-a-sketch functionality

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,4 +1,5 @@
 let squareDimension = 16;
+let currentCursorColor = "grey";
 
 function drawSquareGrid(sideLength) {
     const container = document.querySelector(".container");
@@ -8,6 +9,9 @@ function drawSquareGrid(sideLength) {
         for (let j = 0; j < sideLength; j++) {
             const square = document.createElement("div");
             square.classList.toggle("square");
+            square.addEventListener("mouseover", () => {
+                square.setAttribute("style", `background: ${currentCursorColor}`);
+            })
             col.appendChild(square);
         }
         container.appendChild(col);
