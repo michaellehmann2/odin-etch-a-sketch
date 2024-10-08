0951f6fcc749c329f12fafdb302b51772a279d2a
Michael Lehmann <mikelehmann00@gmail.com>
2024-10-08 15:05:34 -0500

    Split Reset and Change-num into separate buttons
    Split the functionalities of resetting the current sketchpad (now called
    clearing) and changing the number of squares-per-side into two separate
    buttons.
    
    Also cleaned up functions in .js file to align more closely
    with the Single-Responsibility Principle.

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,12 +1,12 @@
-const INIT_SIDE_LENGTH = 16;
+let currentSideLength = 16;
 let currentCursorColor = "grey";
+const container = document.querySelector(".container");
 
-function drawSquareGrid(sideLength) {
-    const container = document.querySelector(".container");
-    for (let i = 0; i < sideLength; i++) {
+function drawSquareGrid() {
+    for (let i = 0; i < currentSideLength; i++) {
         const col = document.createElement("div");
         col.classList.toggle("square-col");
-        for (let j = 0; j < sideLength; j++) {
+        for (let j = 0; j < currentSideLength; j++) {
             const square = document.createElement("div");
             square.classList.toggle("square");
             square.addEventListener("mouseover", () => {
@@ -18,20 +18,33 @@ function drawSquareGrid(sideLength) {
     }
 }
 
-function changeNumSquares() {
-    const container = document.querySelector(".container");
+function clearGrid() {
     //This only works for more modern browsers. It might not work if you're using an older version. Maybe I should address this later?
     container.replaceChildren();
-
-    let newSquareCount;
-    do {
-        newSquareCount = prompt("Please enter a new number of squares-per-side of the grid. Make sure to enter a number between 1 and 100.", 16);
-    }  while (newSquareCount == null || (newSquareCount > 100 || newSquareCount < 1));
-
-    drawSquareGrid(newSquareCount);
 }
 
-drawSquareGrid(INIT_SIDE_LENGTH);
+function clearAndRedraw() {
+    clearGrid();
+    drawSquareGrid();
+}
 
-const resetChangeNumButton = document.querySelector(".reset-change-num-button");
-resetChangeNumButton.addEventListener("click", changeNumSquares);
+function changeNumSquares() {
+    clearGrid();
+
+    let newSideLength;
+    do {
+        newSideLength = prompt(`The current sketchpad is a grid of ${currentSideLength} squares x ${currentSideLength} squares. Enter a new number of squares-per-side of the grid, between 1 and 100.`);
+    }  while (newSideLength == null || (newSideLength > 100 || newSideLength < 1));
+
+    currentSideLength = newSideLength;
+
+    drawSquareGrid();
+}
+
+drawSquareGrid(currentSideLength);
+
+const clearButton = document.querySelector(".clear-button");
+clearButton.addEventListener("click", clearAndRedraw);
+
+const changeNumButton = document.querySelector(".change-num-button");
+changeNumButton.addEventListener("click", changeNumSquares);
